69b4c9c630050bf0a69aa720e7acf09819841563
trung.nguyen <trungab2305@gmail.com>
2024-10-16 17:03:26 +0700

    update

--- 1. k8s/save-scripts.sh
+++ 1. k8s/save-scripts.sh
@@ -1,2 +1,4 @@
+cp get_helm.sh helm-alias.sh k8s-alias.sh save-scripts.sh vim.sh /home/$USER
+cd
 chmod a+x k8s-client.sh helm-alias.sh vim.sh k8s-alias.sh
 ./k8s-client.sh ; ./helm-alias.sh ; ./vim.sh ; ./k8s-alias.sh
